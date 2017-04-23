da5b1478cfd5e5bf20c1b83f295e03507339aae4
jordan <jordan@149-160-180-29.dhcp-bl.indiana.edu>
2017-04-23 12:33:50 -0400

    update1

--- JStats/R/helloWorld.R
+++ JStats/R/helloWorld.R
@@ -1,12 +1,4 @@
-#' Hello world! for JStats
-#' 
-#' This function tests package installation
-#' @param test There is no parameter for this function
-#' @author Jordan Landgrebe
-#' @keywords Hello
-#' @examples 
-#' helloWorld()
-
-helloWorld <- function() { 
+helloWorld <-
+function() { 
   return ("Hello, world!")
   }
